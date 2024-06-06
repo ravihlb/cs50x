d2249ba968184790a9346e36b213ad00b1e1c4df
ravihlb <ravihlb@protonmail.com>
2024-06-06 11:30:06 -0300

    refactor: add src dir, ignore build

---
+++ src/hi.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+  char c1 = 'H';
+  char c2 = 'I';
+  char c3 = '!';
+}
---
+++ src/mario.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+  for (int i = 0; i < 4; i++) {
+    printf("###\n");
+    for (int j = 0; j < 3; j++) {
+      printf("###");
+    }
+  }
+}
