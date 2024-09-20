a19c14ecbb6334a4b47bb2163ba99e2a755975e5
ravihlb <ravihlb@protonmail.com>
2024-09-20 06:53:03 -0300

    feat: `mario.c` v1

--- src/mario.c
+++ src/mario.c
@@ -4,10 +4,12 @@
 int main(void)
 {
 
-    for (int i = 0; i < 4; i++) {
-        printf("###\n");
-        for (int j = 0; j < 3; j++) {
-            printf("###");
+    const int n = get_int("Height: ");
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("#");
         }
+        printf("\n");
     }
 }
