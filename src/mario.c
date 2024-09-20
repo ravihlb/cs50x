0e4ebb2436a8e417035e3473e730ce228ca34729
ravihlb <ravihlb@protonmail.com>
2024-09-20 06:39:41 -0300

    general update

--- src/mario.c
+++ src/mario.c
@@ -1,11 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) {
-  for (int i = 0; i < 4; i++) {
-    printf("###\n");
-    for (int j = 0; j < 3; j++) {
-      printf("###");
+int main(void)
+{
+
+    for (int i = 0; i < 4; i++) {
+        printf("###\n");
+        for (int j = 0; j < 3; j++) {
+            printf("###");
+        }
     }
-  }
 }
